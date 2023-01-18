816714c54db882d94337f58571ac6d7066891443
Eduardo Diez <deduar@gmail.com>
2023-01-18 11:56:13 +0100

    add DDBB config

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'      => '',
         'hostname' => 'localhost',
-        'username' => '',
-        'password' => '',
-        'database' => '',
+        'username' => 'test',
+        'password' => 'P@swd_1a1',
+        'database' => 'test',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
